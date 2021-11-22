8c3ed2424bb1376ce1437fe3019020a236e6f9ea
Matthew Wayock <wayock@gmail.com>
2021-11-22 08:39:02 -0500

    shop preview and initial signin form setup

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -12,7 +12,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">On Sale</span>
+      <span className="subtitle">Shop Now</span>
     </div>
   </div>
 );
